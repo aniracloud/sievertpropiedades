e74690aa1edb13df7a2ab2004145832bffa49d39
mchirino <chimasadus@gmail.com>
2020-09-24 21:39:05 -0300

    funciona antes instalar libreria

--- src/app/components/propiedades/nueva-propiedad/nueva-propiedad.component.ts
+++ src/app/components/propiedades/nueva-propiedad/nueva-propiedad.component.ts
@@ -1,44 +1,55 @@
-import { Component, OnInit, Input, ViewChild, ViewEncapsulation, ElementRef, HostListener } from '@angular/core';
-import { FormGroup, FormControl, FormArray, Validators, FormBuilder, ControlContainer, FormControlName, AbstractControl, PatternValidator } from '@angular/forms';
-import { PropiedadI } from '@shared/models/propiedad.interface';
-import { PropiedadService } from '@components/propiedades/propiedad.service';
-import { UbicacionesService } from '@shared/services/ubicaciones.service';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ViewEncapsulation,
+  ElementRef,
+  HostListener,
+} from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  FormArray,
+  Validators,
+  FormBuilder,
+  ControlContainer,
+  FormControlName,
+  AbstractControl,
+  PatternValidator,
+} from "@angular/forms";
+import { PropiedadI } from "@shared/models/propiedad.interface";
+import { PropiedadService } from "@components/propiedades/propiedad.service";
+import { UbicacionesService } from "@shared/services/ubicaciones.service";
 
-import { HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorResponse } from "@angular/common/http";
 
-import { STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import { STEPPER_GLOBAL_OPTIONS } from "@angular/cdk/stepper";
 
-import { MatAccordion} from '@angular/material/expansion';
-import { concatMapTo, map } from 'rxjs/operators';
-import { getTestBed } from '@angular/core/testing';
-import { FormValidations } from './mode.validator';
-import { keyValuesToMap } from '@angular/flex-layout/extended/typings/style/style-transforms';
-
-
-
-
-
+import { MatAccordion } from "@angular/material/expansion";
+import { concatMapTo, map } from "rxjs/operators";
+import { getTestBed } from "@angular/core/testing";
+import { FormValidations } from "./mode.validator";
+import { keyValuesToMap } from "@angular/flex-layout/extended/typings/style/style-transforms";
 
 @Component({
-  selector: 'app-nueva-propiedad',
-  templateUrl: './nueva-propiedad.component.html',
-  styleUrls: ['./nueva-propiedad.component.scss'],
-  providers: [{
-    provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError: true}
-  }]
+  selector: "app-nueva-propiedad",
+  templateUrl: "./nueva-propiedad.component.html",
+  styleUrls: ["./nueva-propiedad.component.scss"],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true },
+    },
+  ],
 })
-
-
-
 export class NuevaPropiedadComponent implements OnInit {
-
-  @ViewChild ('firstAccordion', { static: true }) firstAccordion: MatAccordion;
-  @ViewChild ('secondAccordion', { static: true }) secondAccordion: MatAccordion;
+  @ViewChild("firstAccordion", { static: true }) firstAccordion: MatAccordion;
+  @ViewChild("secondAccordion", { static: true }) secondAccordion: MatAccordion;
 
   @ViewChild("uf") nameFieldUF: ElementRef;
 
-
-/*
+  /*
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent): void {
     console.log('Codigo del evento ', event.code);
@@ -56,50 +67,68 @@ export class NuevaPropiedadComponent implements OnInit {
 
     }*/
 
-    /*if (!this.isComparisonWindow) {
+  /*if (!this.isComparisonWindow) {
       if (event.key === 'ArrowLeft') {
         this.navigateLeft();
       } else if (event.key === 'ArrowRight') {
         this.navigateRight();
       }*/
 
-
-
   private image: any;
   private ubica: any;
   public region: any;
 
+  modalidad = ["Arriendo", "Venta"];
 
-  modalidad = ['Arriendo', 'Venta'];
-
-
-
-  calefa2: Array<string> = ['Loza Radiante', 'Radiador Ind.', 'Caldera Ind.', 'Caldera Edificio'];
-  orienta2 = ['N', 'NP', 'NO', 'S', 'SO', 'SP'];
-  comunidad2 = ['Gimnacio', 'Zona Juegos', 'Quincho', 'Bicicletero', 'Area Verde', 'Picina'];
+  calefa2: Array<string> = [
+    "Loza Radiante",
+    "Radiador Ind.",
+    "Caldera Ind.",
+    "Caldera Edificio",
+  ];
+  orienta2 = ["N", "NP", "NO", "S", "SO", "SP"];
+  comunidad2 = [
+    "Gimnacio",
+    "Zona Juegos",
+    "Quincho",
+    "Bicicletero",
+    "Area Verde",
+    "Picina",
+  ];
   ItemSelectedValues = [];
-  others_build: string[] = ['cocina americana', 'Estar', 'Logia', 'Patio', 'Home Office'];
-  othersInstallBuild: string[] = ['Conserje', 'Areas Verdes', 'Sauna', 'Ascensor', 'Bicicletero', 'Piscina', 'Estacionamiento Visitas', 'Gimnasio', 'Sala Multiuso', 'Quincho'];
-
-
+  others_build: string[] = [
+    "cocina americana",
+    "Estar",
+    "Logia",
+    "Patio",
+    "Home Office",
+  ];
+  othersInstallBuild: string[] = [
+    "Conserje",
+    "Areas Verdes",
+    "Sauna",
+    "Ascensor",
+    "Bicicletero",
+    "Piscina",
+    "Estacionamiento Visitas",
+    "Gimnasio",
+    "Sala Multiuso",
+    "Quincho",
+  ];
 
   regionesChile: any = null;
   tipoPropiedades: any = null;
   comunasChile: any;
-  comunas: '';
+  comunas: "";
   comunaSelected: string;
   tipoSelected: string;
 
-
-
-  valorPrecioPeso = '';
+  valorPrecioPeso = "";
   valorPrecioUF = null;
   valorcasilla: false;
-  valoruf='';
+  valoruf = "";
 
-
-  public build_floor = '';
-
+  public build_floor = "";
 
   public valorUF: any;
 
@@ -107,252 +136,235 @@ export class NuevaPropiedadComponent implements OnInit {
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
 
-
-
-  constructor(  private propiedadSvc: PropiedadService,
-                public UbicacionSvc: UbicacionesService,
-                private _formBuilder: FormBuilder
-                ) {}
-
+  constructor(
+    private propiedadSvc: PropiedadService,
+    public UbicacionSvc: UbicacionesService,
+    private _formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
-
     this.firstFormGroup = this._formBuilder.group({
-      titulo:           ['', [Validators.required,
-                            Validators.minLength(5),
-                            Validators.maxLength(30),
-                            Validators.pattern('^[A-Za-z0-9ñÑáéíóúÁÉÍÓÚ ]+$')]],
-      direccion:       ['', Validators.required],
-      regionSelected:  ['', Validators.required],
-      comunaSelected:  ['', Validators.required],
-      codigo:          ['', Validators.pattern('\\-?\\d*\\.?\\d{1,2}')],
-      typeSelected:    ['', Validators.required],
-      modalidad:       this.buildModalidad()
-     // modalidad: this._formBuilder.group({
+      titulo: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(30),
+          Validators.pattern("^[A-Za-z0-9ñÑáéíóúÁÉÍÓÚ ]+$"),
+        ],
+      ],
+      direccion: ["", Validators.required],
+      regionSelected: ["", Validators.required],
+      comunaSelected: ["", Validators.required],
+      codigo: ["", Validators.pattern("\\-?\\d*\\.?\\d{1,2}")],
+      typeSelected: ["", Validators.required],
+      modalidad: this.buildModalidad(),
+      // modalidad: this._formBuilder.group({
       //  rent:    [''],
       //  sales:    ['']
-      });
-
+    });
 
     this.secondFormGroup = this._formBuilder.group({
-      building:       [''],
-      old_build:      [''],
-      met_utiles:     [''],
-      build_floor:    ['', Validators.pattern('^[0-9]+$')],
+      building: [""],
+      old_build: [""],
+      met_utiles: [""],
+      build_floor: ["", Validators.pattern("^[0-9]+$")],
 
-      bedroom:        [''],
-      bath:           [''],
-      build_terrace:  [''],
-      store:          [''],
+      bedroom: [""],
+      bath: [""],
+      build_terrace: [""],
+      store: [""],
 
+      orienta: [""],
+      control: [""],
 
-      orienta:      [''],
-      control:      [''],
-
-
-      description:  ['', Validators.required]
-
+      description: ["", Validators.required],
     });
-
 
     this.thirdFormGroup = this._formBuilder.group({
-      valorPrecioUF:    ['', Validators.required],
-      valorPrecioPeso:  ['', Validators.required]
+      valorPrecioUF: ["", Validators.required],
+      valorPrecioPeso: ["", Validators.required],
     });
 
+    this.UbicacionSvc.getRegiones().subscribe(
+      (dataRegiones) => (this.regionesChile = dataRegiones),
+      (err: HttpErrorResponse) => {
+        console.log("hay un error");
+        console.log(err.status);
+      },
+      () => {
+        console.log("fin Regiones");
+      }
+    );
 
-    this.UbicacionSvc.getRegiones()
-        .subscribe(dataRegiones => (this.regionesChile = dataRegiones),
-                    (err: HttpErrorResponse ) => {
-                        console.log('hay un error');
-                        console.log(err.status);
-                    },
-                    () => { console.log('fin Regiones'); }
-        );
+    this.UbicacionSvc.getTipo().subscribe(
+      (dataTipo) => (this.tipoPropiedades = dataTipo),
+      (err: HttpErrorResponse) => {
+        console.log("hay un error");
+        console.log(err.status);
+      },
+      () => {
+        console.log("fin Tipo");
+      }
+    );
 
+    this.UbicacionSvc.getEconomico().subscribe(
+      (dataUF) => (this.valorUF = dataUF),
+      (err: HttpErrorResponse) => {
+        console.log("hay un error");
+        console.log(err.status);
+      },
+      () => {
+        console.log("fin Economico");
+      }
+    );
 
-    this.UbicacionSvc.getTipo()
-        .subscribe(dataTipo => (this.tipoPropiedades = dataTipo),
-                    (err: HttpErrorResponse) => {
-                        console.log('hay un error');
-                        console.log(err.status);
-                    },
-                    () => { console.log('fin Tipo'); }
-        );
-
-
-    this.UbicacionSvc.getEconomico()
-        .subscribe(dataUF => (this.valorUF = dataUF),
-                    (err: HttpErrorResponse) => {
-                        console.log('hay un error');
-                        console.log(err.status);
-                    },
-                    () => { console.log('fin Economico'); }
-        );
-
-       // this.getFieldNumber();
+    // this.getFieldNumber();
   }
 
-  buildModalidad(){
-    const values = this.modalidad.map(value => new FormControl(false));
+  buildModalidad() {
+    const values = this.modalidad.map((value) => new FormControl(false));
     //console.log(values[1]);
-    return this._formBuilder.array(values,FormValidations.requiredMinCheckbox(1));
+    return this._formBuilder.array(
+      values,
+      FormValidations.requiredMinCheckbox(1)
+    );
     /* this._formBulder.array([
       new FormControl(false),
       new FormControl(false)
     ]);*/
   }
 
-
-
   getModalidadControl() {
-    return this.firstFormGroup.get('modalidad') ? (<FormArray>this.firstFormGroup.get('modalidad')).controls : null;
+    return this.firstFormGroup.get("modalidad")
+      ? (<FormArray>this.firstFormGroup.get("modalidad")).controls
+      : null;
   }
 
-
-
-
-
-
-  OnChange($event): any{
+  OnChange($event): any {
     // MatCheckboxChange {checked,MatCheckbox}
-   console.log($event.values);
-   console.log($event);
-   if ($event){
-    return this.firstFormGroup.valid;
+    console.log($event.values);
+    console.log($event);
+    if ($event) {
+      return this.firstFormGroup.valid;
+    }
   }
 
+  onSubmit() {
+    console.log("Valores => " + JSON.stringify(this.firstFormGroup.value));
   }
-
-  onSubmit(){
-    console.log('Valores => ' + JSON.stringify(this.firstFormGroup.value));
-  }
-
 
   addNewPropiedad(data: PropiedadI) {
-    console.log('New post', data);
+    console.log("New post", data);
     // this.propiedadSvc.preAddAndUpdatePropiedad(data, this.image);
   }
 
-
-
   handleimage(event: any): void {
-      this.image = event.target.files[0];
+    this.image = event.target.files[0];
   }
 
-
-
-
   getTecla(event: KeyboardEvent, tipo: string) {
-    if ((event.key === 'Enter' && tipo === 'uf')) {
-      console.log (this.valorPrecioUF);
-      console.log (this.valorUF.uf.valor);
-      console.log('******************');
-      this.valorPrecioPeso = ((+this.valorPrecioUF) * (+this.valorUF.uf.valor)).toLocaleString('es', { maximumFractionDigits: 0 });
+    if (event.key === "Enter" && tipo === "uf") {
+      console.log(this.valorPrecioUF);
+      console.log(this.valorUF.uf.valor);
+      console.log("******************");
+      this.valorPrecioPeso = (
+        +this.valorPrecioUF * +this.valorUF.uf.valor
+      ).toLocaleString("es", { maximumFractionDigits: 0 });
     } else {
-      if ((event.key === 'Enter' && tipo === 'peso')) {
-        this.valorPrecioUF = ((+this.valorPrecioPeso) / (+this.valorUF.uf.valor)).toFixed();
-        this.valorPrecioPeso = (+this.valorPrecioPeso).toLocaleString('es', { maximumFractionDigits: 0 });
+      if (event.key === "Enter" && tipo === "peso") {
+        this.valorPrecioUF = (
+          +this.valorPrecioPeso / +this.valorUF.uf.valor
+        ).toFixed();
+        this.valorPrecioPeso = (+this.valorPrecioPeso).toLocaleString("es", {
+          maximumFractionDigits: 0,
+        });
       }
     }
     this.UbicacionSvc.check(event);
   }
 
-
-  editName(event: KeyboardEvent){
-   // this.nameFieldUF.nativeElement.focus();
-   this.thirdFormGroup.get('valorPrecioUF').valueChanges
-   .subscribe(val=>{
-
-
-     console.log(val);
-   });
-
+  editName(event: KeyboardEvent) {
+    // this.nameFieldUF.nativeElement.focus();
+    this.thirdFormGroup.get("valorPrecioUF").valueChanges.subscribe((val) => {
+      console.log(val);
+    });
   }
 
-
-
-
-
-  getFieldNumber(evt:KeyboardEvent , tipo: string): any{
-      if(tipo === 'uf'){
-          if(this.thirdFormGroup.get('valorPrecioUF').value === null){
-            this.valorPrecioPeso = null;
-            return;
-          }
-          const valoruf2 = this.thirdFormGroup.get('valorPrecioUF').value;
-          const valor3 = valoruf2.toString();
-          if((valor3.length) > 0 && valor3 != '0'){
-            if ((evt.code === 'Enter' )) {
-              console.log('Valor luego de presionar enter: ',valor3);
-              return;
-            }
-            else{
-              if(valoruf2 < 0){
-                this.valorPrecioUF = null;
-                this.valorPrecioPeso = null;
-              }else{
-                this.valorPrecioPeso = (((+this.valorPrecioUF) * (+this.valorUF.uf.valor)).toFixed());
-              }
-            }
-          } else {
-            this.valorPrecioUF = null;
-            this.valorPrecioPeso = null;
-          }
-          if(evt.code === 'Enter' || evt.code === 'NumpadEnter' ){
-            console.log('se ha presionado enter o enterpad');
-          }
-          return;
-      }
-      if(tipo === 'peso'){
-        console.log(evt.code);
-        if(this.thirdFormGroup.get('valorPrecioPeso').value === null){
-          this.valorPrecioUF = null;
-          return;
-        }
-        const valorpeso = this.thirdFormGroup.get('valorPrecioPeso').value;
-        const valor = valorpeso.toString();
-        if((valor.length) > 0 && valor != '0'){
-          console.log('presiono enter ', valorpeso);
-          console.log('codigo enter ', evt.code);
-          if(evt.code === 'Enter' || evt.code === 'NumpadEnter' ){
-            console.log('se ha presionado enter o enterpad');
-          }
-          if(valorpeso < 1){
-            this.valorPrecioUF = null;
-            this.valorPrecioPeso = null;
-          }else{
-            const x = ((+this.valorPrecioPeso) / (this.valorUF.uf.valor)).toPrecision(3);
-            this.valorPrecioUF = +x;
-            return;
-          }
-        } else {
-          this.valorPrecioUF = '';
-          this.valorPrecioPeso = '';
-        }
+  getFieldNumber(evt: KeyboardEvent, tipo: string): any {
+    if (tipo === "uf") {
+      if (this.thirdFormGroup.get("valorPrecioUF").value === null) {
+        this.valorPrecioPeso = null;
         return;
       }
+      const valoruf2 = this.thirdFormGroup.get("valorPrecioUF").value;
+      const valor3 = valoruf2.toString();
+      if (valor3.length > 0 && valor3 != "0") {
+        if (evt.code === "Enter") {
+          console.log("Valor luego de presionar enter: ", valor3);
+          return;
+        } else {
+          if (valoruf2 < 0) {
+            this.valorPrecioUF = null;
+            this.valorPrecioPeso = null;
+          } else {
+            this.valorPrecioPeso = (
+              +this.valorPrecioUF * +this.valorUF.uf.valor
+            ).toFixed();
+          }
+        }
+      } else {
+        this.valorPrecioUF = null;
+        this.valorPrecioPeso = null;
+      }
+      if (evt.code === "Enter" || evt.code === "NumpadEnter") {
+        console.log("se ha presionado enter o enterpad");
+      }
+      return;
+    }
+    if (tipo === "peso") {
+      console.log(evt.code);
+      if (this.thirdFormGroup.get("valorPrecioPeso").value === null) {
+        this.valorPrecioUF = null;
+        return;
+      }
+      const valorpeso = this.thirdFormGroup.get("valorPrecioPeso").value;
+      const valor = valorpeso.toString();
+      if (valor.length > 0 && valor != "0") {
+        console.log("presiono enter ", valorpeso);
+        console.log("codigo enter ", evt.code);
+        if (evt.code === "Enter" || evt.code === "NumpadEnter") {
+          console.log("se ha presionado enter o enterpad");
+        }
+        if (valorpeso < 1) {
+          this.valorPrecioUF = null;
+          this.valorPrecioPeso = null;
+        } else {
+          const x = (+this.valorPrecioPeso / this.valorUF.uf.valor).toPrecision(
+            3
+          );
+          this.valorPrecioUF = +x;
+          return;
+        }
+      } else {
+        this.valorPrecioUF = "";
+        this.valorPrecioPeso = "";
+      }
+      return;
+    }
   }
 
-
-  valida(){
-    console.log('validando el formulario');
+  valida() {
+    console.log("validando el formulario");
   }
-
-
 
   borracampo() {
-    console.log('Estamos en borrar campo');
-    this.valorPrecioPeso = '';
+    console.log("Estamos en borrar campo");
+    this.valorPrecioPeso = "";
     this.valorPrecioUF = null;
   }
 
-
-
-
-  activa() {
-
-  }
+  activa() {}
 
   getItemSelectedValue() {
     this.ItemSelectedValues = [];
@@ -364,13 +376,9 @@ export class NuevaPropiedadComponent implements OnInit {
     console.log(this.ItemSelectedValues);
   }
 
-
-  addFruitsControls() {
-
-  }
+  addFruitsControls() {}
 
   getComunaSelectedValue(region: string) {
     this.comunasChile = region;
   }
-
 }
--- src/app/components/propiedades/nueva-propiedad/nueva-propiedad.module.ts
+++ src/app/components/propiedades/nueva-propiedad/nueva-propiedad.module.ts
@@ -1,19 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
-import { FlexLayoutModule } from '@angular/flex-layout';
-
-
-
-
-
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [],
-  imports: [ReactiveFormsModule, FormsModule, FlexLayoutModule,
-    CommonModule
-  ],
-  exports: []
+  imports: [ReactiveFormsModule, FormsModule, FlexLayoutModule, CommonModule],
+  exports: [],
 })
-export class NuevaPropiedadModule { }
+export class NuevaPropiedadModule {}
